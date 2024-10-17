e502417dec659c83fe1bf3af61ea31365ec58940
Daniel Shiposha <ds@unique.network>
2024-10-17 13:01:02 +0200

    chore: xcm nft dev env chains examples

--- transfer-fungible.example.ts
+++ transfer-fungible.example.ts
@@ -1,91 +1,67 @@
 import {Registry, parachainUniversalLocation, relaychainUniversalLocation} from '@open-xcm-tools/simple-xcm';
 import {location, universalLocation} from '@open-xcm-tools/xcm-util/common';
 
-const BDK_URL = process.env.BDK_URL!;
-  
 void (async () => {
-    const registry = new Registry()
-      .addChain({
-        identity: {
-          name: 'Polkadot',
-          universalLocation: relaychainUniversalLocation('westend'),
+  const registry = new Registry()
+  .addChain({
+    identity: {
+      name: 'Relay',
+      universalLocation: relaychainUniversalLocation('westend'),
+    },
+    endpoints: ['wss://xnft-relay.unique.network'],
+  })
+  .addChain({
+    identity: {
+      name: 'AssetHub',
+      universalLocation: parachainUniversalLocation('westend', 1000n),
+    },
+    endpoints: ['wss://xnft-assethub.unique.network'],
+  })
+  .addChain({
+    identity: {
+      name: 'Unique',
+      universalLocation: parachainUniversalLocation('westend', 2037n),
+    },
+    endpoints: ['wss://xnft-unique.unique.network'],
+  })
+  .addCurrency({
+    symbol: 'xUSD',
+    decimals: 6,
+    universalLocation: universalLocation('westend', [
+      {parachain: 1000n},
+      {palletInstance: 50n},
+      {generalIndex: 32n},
+    ]),
+  })
+  .addRelativeLocation(
+    'TestAccount',
+    location(0n, [
+      {
+        accountId32: {
+          id: '5HMqkp4Zo9oYrWAL2jhi93xSbcLFhfakaqzpomuMTwDQUfMz',
         },
-        endpoints: [`${BDK_URL}/relay/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubA',
-          universalLocation: parachainUniversalLocation('westend', 2001n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubA/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubB',
-          universalLocation: parachainUniversalLocation('westend', 2002n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubB/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubC',
-          universalLocation: parachainUniversalLocation('westend', 2003n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubC/`],
-      })
-      .addCurrency({
-        symbol: 'AHST',
-        decimals: 6,
-        universalLocation: universalLocation('westend', [
-          {parachain: 2001n},
-          {palletInstance: 50n},
-          {generalIndex: 1984n},
-        ]),
-      })
-      .addRelativeLocation(
-        'Alice',
-        location(0n, [
-          {
-            accountId32: {
-              id: '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d',
-            },
-          },
-        ]),
-      );
+      },
+    ]),
+  );
 
-    await registry.addNativeCurrency('Polkadot');
+  await registry.addNativeCurrency('Relay');
+  await registry.addNativeCurrency('Unique');
 
-    let xcm = await registry.connectXcm('AssetHubA');
-    console.log('XCM version:', xcm.xcmVersion);
+  let xcm = await registry.connectXcm('AssetHub');
+  console.log('XCM version:', xcm.xcmVersion);
 
-    let transferTx = await xcm.composeTransfer({
-      origin: 'Alice',
-      assets: [
-        xcm.adjustedFungible('AHST', '30'),
-      ],
-      feeAssetId: 'AHST',
-      destination: 'AssetHubB',
-      beneficiary: 'Alice',
-    });
+  let transferTx = await xcm.composeTransfer({
+    origin: 'TestAccount',
+    assets: [
+      xcm.adjustedFungible('xUSD', '10'),
+    ],
+    feeAssetId: 'xUSD',
+    destination: 'Unique',
+    beneficiary: 'TestAccount',
+  });
 
-    console.log(transferTx.method.toHex());
+  console.log('the composed extrinsic:', transferTx.method.toHex());
 
-    await xcm.disconnect();
-
-    xcm = await registry.connectXcm('AssetHubB');
-    console.log('XCM version:', xcm.xcmVersion);
-
-    transferTx = await xcm.composeTransfer({
-      origin: 'Alice',
-      assets: [
-        xcm.adjustedFungible('AHST', '10'),
-      ],
-      feeAssetId: 'AHST',
-      destination: 'AssetHubC',
-      beneficiary: 'Alice',
-    });
-
-    console.log(transferTx.method.toHex());
-
+  await xcm.disconnect();
 })();
-  
+
--- transfer-non-fungible.example.ts
+++ transfer-non-fungible.example.ts
@@ -1,90 +1,76 @@
 import {Registry, parachainUniversalLocation, relaychainUniversalLocation} from '@open-xcm-tools/simple-xcm';
 import {asset, location, nonfungible, universalLocation} from '@open-xcm-tools/xcm-util/common';
 
-const BDK_URL = process.env.BDK_URL!;
-  
 void (async () => {
-    const registry = new Registry()
-      .addChain({
-        identity: {
-          name: 'Polkadot',
-          universalLocation: relaychainUniversalLocation('westend'),
+  const registry = new Registry()
+  .addChain({
+    identity: {
+      name: 'Relay',
+      universalLocation: relaychainUniversalLocation('westend'),
+    },
+    endpoints: ['wss://xnft-relay.unique.network'],
+  })
+  .addChain({
+    identity: {
+      name: 'AssetHub',
+      universalLocation: parachainUniversalLocation('westend', 1000n),
+    },
+    endpoints: ['wss://xnft-assethub.unique.network'],
+  })
+  .addChain({
+    identity: {
+      name: 'Unique',
+      universalLocation: parachainUniversalLocation('westend', 2037n),
+    },
+    endpoints: ['wss://xnft-unique.unique.network'],
+  })
+  .addCurrency({
+    symbol: 'xUSD',
+    decimals: 6,
+    universalLocation: universalLocation('westend', [
+      {parachain: 1000n},
+      {palletInstance: 50n},
+      {generalIndex: 32n},
+    ]),
+  })
+  .addUniversalLocation(
+    'AssetHubNftCollection',
+    universalLocation('westend', [
+      {parachain: 1000n},
+      {palletInstance: 51n},
+      {generalIndex: 21n},
+    ]),
+  )
+  .addRelativeLocation(
+    'TestAccount',
+    location(0n, [
+      {
+        accountId32: {
+          id: '5HMqkp4Zo9oYrWAL2jhi93xSbcLFhfakaqzpomuMTwDQUfMz',
         },
-        endpoints: [`${BDK_URL}/relay/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubA',
-          universalLocation: parachainUniversalLocation('westend', 2001n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubA/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubB',
-          universalLocation: parachainUniversalLocation('westend', 2002n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubB/`],
-      })
-      .addChain({
-        identity: {
-          name: 'AssetHubC',
-          universalLocation: parachainUniversalLocation('westend', 2003n),
-        },
-        endpoints: [`${BDK_URL}/relay-assethubC/`],
-      })
-      .addUniversalLocation(
-        'NFTCollection',
-        universalLocation('westend', [
-          {parachain: 2001n},
-          {palletInstance: 51n},
-          {generalIndex: 42n},
-        ]),
-      )
-      .addRelativeLocation(
-        'Alice',
-        location(0n, [
-          {
-            accountId32: {
-              id: '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d',
-            },
-          },
-        ]),
-      );
+      },
+    ]),
+  );
 
-    await registry.addNativeCurrency('Polkadot');
+  await registry.addNativeCurrency('Relay');
+  await registry.addNativeCurrency('Unique');
 
-    let xcm = await registry.connectXcm('AssetHubA');
-    console.log('XCM version:', xcm.xcmVersion);
+  let xcm = await registry.connectXcm('AssetHub');
+  console.log('XCM version:', xcm.xcmVersion);
 
-    let transferTx = await xcm.composeTransfer({
-      origin: 'Alice',
-      assets: [
-        asset('NFTCollection', nonfungible(112n)),
-      ],
-      feeAssetId: 'AHST',
-      destination: 'AssetHubB',
-      beneficiary: 'Alice',
-    });
+  let transferTx = await xcm.composeTransfer({
+    origin: 'TestAccount',
+    assets: [
+      asset('AssetHubNftCollection', nonfungible(111n)),
+      xcm.adjustedFungible('xUSD', '10'),
+    ],
+    feeAssetId: 'xUSD',
+    destination: 'Unique',
+    beneficiary: 'TestAccount',
+  });
 
-    console.log(transferTx.method.toHex());
+  console.log('the composed extrinsic', transferTx.method.toHex());
 
-    await xcm.disconnect();
-
-    xcm = await registry.connectXcm('AssetHubB');
-    console.log('XCM version:', xcm.xcmVersion);
-
-    transferTx = await xcm.composeTransfer({
-      origin: 'Alice',
-      assets: [
-        asset('NFTCollection', nonfungible(12n)),
-      ],
-      feeAssetId: 'AHST',
-      destination: 'AssetHubC',
-      beneficiary: 'Alice',
-    });
-
-    console.log(transferTx.method.toHex());
-
+  await xcm.disconnect();
 })();
-    
+
